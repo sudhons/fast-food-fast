45dd82f2a2eecd0e09ff6aa2b848b8da5cac01f9
Oluwaseun Jolaoso <sudhons@gmail.com>
2018-09-12 16:20:54 +0100

    create queries for querying data

--- API/src/db/queries/orderQueries.js
+++ API/src/db/queries/orderQueries.js
@@ -1,8 +1,42 @@
 import orderData from '../dbData';
 
 class Data {
+  constructor(customer, recipientName, recipientAddr, recipientPhone, order) {
+    Data.count = Data.count ? Data.count + 1 : 1;
+
+    this.orderId = Data.count;
+    this.customer = customer;
+    this.recipientName = recipientName;
+    this.recipientAddress = recipientAddr;
+    this.recipientPhone = recipientPhone;
+    this.order = order;
+    this.orderTime = Date.now();
+    this.status = 'waiting';
+  }
+
+  static createNewOrder(customer, recipientName, recipientAddr, recipientPhone, order) {
+    const newOrder = new Data(customer, recipientName, recipientAddr, recipientPhone, order);
+    orderData.push(newOrder);
+
+    return newOrder || null;
+  }
+
   static getAllOrders() {
     return orderData;
+  }
+
+  static getAnOrder(orderId) {
+    const requiredOrder = orderData.find(order => order.orderId === orderId);
+    return requiredOrder || null;
+  }
+
+  static updateOrderStatus(orderId, status) {
+    const requiredOrder = Data.getAnOrder(orderId);
+    if (requiredOrder) {
+      requiredOrder.status = status;
+    }
+
+    return requiredOrder || null;
   }
 
   static deleteAllOrders() {
