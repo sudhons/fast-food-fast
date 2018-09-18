b1288e6686ed4b7398c1476ab5ad06dc5d44130f
Oluwaseun Jolaoso <sudhons@gmail.com>
2018-09-18 17:07:58 +0100

    [Chore #160592765] Implement feedback on UI codes

--- UI/js/orderHistory.js
+++ UI/js/orderHistory.js
@@ -1,12 +1,26 @@
-const orderDetail = (e) => {
-  e.preventDefault();
-  const orderDetailSection = e.target.parentElement.parentElement.nextElementSibling;
+const orderDetail = (event) => {
+  event.preventDefault();
+  const orderDetailSection = event
+    .target.parentElement.parentElement.nextElementSibling;
   if (orderDetailSection.style.height === '25rem') {
     orderDetailSection.style.height = '';
   } else {
     orderDetailSection.style.height = '25rem';
   }
 };
+
+closeBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  menuBar.style.width = '0';
+  openBtn.style.display = 'block';
+});
+
+openBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  menuBar.style.display = 'block';
+  menuBar.style.width = '14rem';
+  openBtn.style.display = 'none';
+});
 
 document.querySelectorAll('.show-order')
   .forEach(element => element.addEventListener('click', orderDetail));
