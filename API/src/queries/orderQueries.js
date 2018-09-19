b84a03614993d6ee2a72166e8a97a4054fa4632b
Oluwaseun Jolaoso <sudhons@gmail.com>
2018-09-19 12:54:15 +0100

    [Bug #160616909] Implement API feedback
    Update README.md
    
    Update README.md

--- API/src/queries/orderQueries.js
+++ API/src/queries/orderQueries.js
@@ -1,6 +1,18 @@
 import orderData from '../db/dbData';
 
+/**
+ * queries data
+ */
 class Data {
+  /**
+   * @constructs
+   * @description constructs a new Data
+   * @param {string} customer - customer name
+   * @param {string} recipientName - recipient name
+   * @param {string} recipientAddr - recipient address
+   * @param {number} recipientPhone - recipient phone number
+   * @param {array} order - an array of meal
+   */
   constructor(customer, recipientName, recipientAddr, recipientPhone, order) {
     Data.count = Data.count ? Data.count + 1 : 1;
 
@@ -14,6 +26,16 @@ class Data {
     this.status = 'waiting';
   }
 
+  /**
+   * @static
+   * @method createNewOrder
+   * @description creates a new order
+   * @param {string} recipientName - the recipient name
+   * @param {string} recipientAddr - the recipient address
+   * @param {number} recipientPhone - the recipient phone number
+   * @param {array} order - an array of meal
+   * @returns {object} the new order
+   */
   static createNewOrder(recipientName, recipientAddr, recipientPhone, order) {
     const customer = 'Adeolu James';
     const unitPrice = 200;
@@ -22,32 +44,63 @@ class Data {
       mealOrder.unitPrice = unitPrice;
       mealOrder.total = value.quantity * unitPrice;
     });
-    const newOrder = new Data(customer, recipientName, recipientAddr, recipientPhone, order);
+    const newOrder = new Data(
+      customer,
+      recipientName,
+      recipientAddr,
+      recipientPhone,
+      order
+    );
     orderData.push(newOrder);
 
     return newOrder;
   }
 
+  /**
+   * @static
+   * @method getAllOrders
+   * @description Fetches all the orders
+   * @returns {Array} an array of all orders
+   */
   static getAllOrders() {
     return orderData;
   }
 
+  /**
+   * @static
+   * @method getAnOrder
+   * @description Fetches an order by its id
+   * @param {integer} orderId - the order's ID
+   * @returns {object} an order or null if order id does not exist
+   */
   static getAnOrder(orderId) {
     const requiredOrder = orderData.find(order => order.orderId === orderId);
     return requiredOrder || null;
   }
 
+  /**
+   * @static
+   * @method getAnOrder
+   * @description updates the status of an order
+   * @param {integer} orderId - the order's ID
+   * @param {string} status - the new status
+   * @returns {object} the updated order
+   */
   static updateOrderStatus(orderId, status) {
     const requiredOrder = Data.getAnOrder(orderId);
 
-    if (requiredOrder) {
-      requiredOrder.status = status;
-      requiredOrder[`${status}Time`] = Date.now();
-    }
+    requiredOrder.status = status;
+    requiredOrder[`${status}Time`] = Date.now();
 
-    return requiredOrder || null;
+    return requiredOrder;
   }
 
+  /**
+   * @static
+   * @method deleteAllOrders
+   * @description delete all orders
+   * @returns {undefined} undefined
+   */
   static deleteAllOrders() {
     orderData.length = 0;
   }
